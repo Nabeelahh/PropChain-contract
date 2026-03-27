39ad5181b52d4bb7ddc8333d74368ac42ea1acfb
Nabeelahh <Nabeelahh@users.noreply.github.com>
2026-03-27 18:25:26 +0000

    deploy: 056ea99da5b4d47d7cc94386a08698a8ecd0e4c8

---
+++ trait.impl/ink_storage_traits/storage/trait.Storable.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["propchain_insurance",[["impl Storable for <a class=\"struct\" href=\"propchain_insurance/struct.PropertyInsurance.html\" title=\"struct propchain_insurance::PropertyInsurance\">PropertyInsurance</a>",0]]],["propchain_traits",[["impl Storable for <a class=\"struct\" href=\"propchain_traits/access_control/struct.AccessControl.html\" title=\"struct propchain_traits::access_control::AccessControl\">AccessControl</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[206,217]}
